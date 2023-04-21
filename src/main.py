1702f287e05916b095a3102e8e299c287c8dfd36
Artyom <sheludeshev.artyom@mail.ru>
2023-04-21 23:41:37 +0300

    Added celery for SMTP messages

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
 from src.operations.router import router as router_operation
+from src.tasks.router import router as router_tasks
 
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
@@ -25,6 +26,10 @@ app.include_router(
 
 app.include_router(
     router_operation
+)
+
+app.include_router(
+    router_tasks
 )
 
 @app.on_event("startup")
